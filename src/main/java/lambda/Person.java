55f16550f666b09f21a4562874657ced864c3dc0
tuonioooo <daizhaoman@sina.com>
2018-05-08 17:33:13 +0800

    lambda表达式教程

---
+++ src/main/java/lambda/Person.java
@@ -0,0 +1,16 @@
+package lambda;
+
+/**
+* @author tuonioooo
+*/
+public class Person {
+    public String firstName;
+    public String lastName;
+
+    public Person() {}
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
